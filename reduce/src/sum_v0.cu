013878f884e6a8e243813a52d2c4b6f0b07898e2
gaojian zhang <gaojian.zhang@gua.com>
2026-04-28 14:46:41 +0800

    feature(reduce sum): update sum_v0 for add benchmark
    Signed-off-by: gaojian zhang <gaojian.zhang@gua.com>

--- reduce/src/sum_v0.cu
+++ reduce/src/sum_v0.cu
@@ -29,6 +29,31 @@ __global__ void sum_v0(float *din, float *dout, int n)
     *dout = sum;
 }
 
+template<typename T>
+float benchmark_kernel(T func, int repeats, int warpup = 1)
+{
+    float time = 0.0;
+    if (repeats <= 0) return time;
+    for (int i = 0; i < warmup; ++i) {
+        func();
+    }
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
+    for (int i = 0;i < repeats; ++i) {
+        func();
+    }
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&time, start, stop);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    return time / repeats;
+}
+
 int main()
 {
     constexpr int N = 10000000;
@@ -46,16 +71,7 @@ int main()
     cudaMalloc((void**)&dout, sizeof(float));
     cudaMemset(dout, 0, sizeof(float));
     cudaMemcpy(din, hin, N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start);
     sum_v0<<<BLOCK_SIZE, GRID_SIZE>>>(din, dout, N);
-    cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
-    float time;
-    cudaEventElapsedTime(&time, start, stop);
-    printf("time = %f\n", time);
     sum_cpu(hin, hout, N);
     float *dout_cpu = (float*)malloc(sizeof(float));
     memset(dout_cpu, 0, sizeof(float));
@@ -65,6 +81,9 @@ int main()
     } else {
         printf("test failed, hout = %f, dout_cpu = %f\n", hout, dout_cpu);
     }
+    auto sum_v1_kernel = [&](){sum_v1<<<BLOCK_SIZE, GRID_SIZE>>>(din, dout, N);};
+    float time = benchmark_kernel(sum_v1_kernel, 5, 2);
+    printf("time = %f\n", time);
     cudaFree(din);
     cudaFree(dout);
     free(hin);
